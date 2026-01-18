aa9d02aa03883bf06b656b7d5563bbcdc19ef1f8
everpcpc <everpcpc@users.noreply.github.com>
2026-01-18 19:19:22 +0800

    fix: change LoadingIcon to ProgressView (#327)

--- KMReader/Shared/UI/LoadingIcon.swift
+++ KMReader/Shared/UI/LoadingIcon.swift
@@ -7,14 +7,17 @@ import SwiftUI
 
 struct LoadingIcon: View {
   var body: some View {
-    if #available(iOS 18.0, macOS 15.0, tvOS 18.0, *) {
-      Image(systemName: "arrow.clockwise.circle")
-        .symbolEffect(.rotate.byLayer, options: .repeat(.periodic(delay: 0.0)))
-        .foregroundStyle(.secondary)
-    } else {
-      Image(systemName: "arrow.clockwise.circle")
-        .animation(.easeInOut(duration: 1).repeatForever(), value: 0)
-        .foregroundStyle(.secondary)
-    }
+    ProgressView()
+      .progressViewStyle(.circular)
+      .tint(.secondary)
+    // if #available(iOS 18.0, macOS 15.0, tvOS 18.0, *) {
+    //   Image(systemName: "arrow.clockwise.circle")
+    //     .symbolEffect(.rotate.byLayer, options: .repeat(.periodic(delay: 0.0)))
+    //     .foregroundStyle(.secondary)
+    // } else {
+    //   Image(systemName: "arrow.clockwise.circle")
+    //     .animation(.easeInOut(duration: 1).repeatForever(), value: 0)
+    //     .foregroundStyle(.secondary)
+    // }
   }
 }
